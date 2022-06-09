1c5e63651f9ac842fba06961ad6c149ede1e5648
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-06-09 14:01:37 +0200

    Fixet header + burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const faviconEl = document.querySelector('link [rel="icon"]');
 const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 mediaQuery.addEventListener("change", themeChange);
 
-// listener 👂
+// listener
 function themeChange(event) {
   if (event.matches) {
     faviconEl.setAttribute("href", "vaabenskjold_fav_light.svg");
@@ -15,3 +15,47 @@ function themeChange(event) {
     faviconEl.setAttribute("href", "vaabenskjold_dark.svg");
   }
 }
+
+// ====== BURGER MENU ======
+
+// Constants
+const menu = document.querySelector(".menu-ul");
+const menuItems = document.querySelectorAll(".menuItem");
+const burger = document.querySelector(".burger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+window.addEventListener("load", tjekBurger);
+window.addEventListener("resize", tjekBurger);
+
+//Remove Buger if wondow over 800px
+function tjekBurger() {
+  if (window.matchMedia("(min-width:800px)").matches) {
+    console.log("vinduet er 800px +");
+    burger.classList.add("hide");
+    menu.classList.remove("menu");
+    menu.classList.remove("transition-burger");
+  } else {
+    console.log("vinduet er 799px -");
+    burger.classList.remove("hide");
+    menu.classList.add("menu");
+  }
+}
+
+// Function for toggle
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    menu.classList.add("transition-burger");
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    menu.classList.add("transition-burger");
+  }
+}
+
+//Event listener
+burger.addEventListener("click", toggleMenu);
